86f1bde8b83344ad3a12ed5bf6117725b6450108
EricLiMac <3270793295@qq.com>
2024-12-03 15:46:22 +0800

    finish loading file

--- fla-project/main.cpp
+++ fla-project/main.cpp
@@ -69,7 +69,6 @@ struct Args parse_args(int argc, char *argv[], Logger &logger)
       exit(0);
     } else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
       args.verbose = true;
-      logger.setLogToStderr(true);
     } else {
       if (paresd_all) {
         logger << "unexpected argument: " << argv[i] << endl;
@@ -91,14 +90,60 @@ struct Args parse_args(int argc, char *argv[], Logger &logger)
   return args;
 }
 
+// Start file reading
+#include <fstream>
+
+enum fla_type
+{
+  TM,
+  PDA
+};
+
+struct fla_content
+{
+  fla_type type;
+  string   content;
+};
+
+struct fla_content load_fla_file(const string &file_path)
+{
+  struct fla_content ret;
+  if (file_path.substr(file_path.find_last_of(".") + 1) == "tm") {
+    ret.type = TM;
+  } else if (file_path.substr(file_path.find_last_of(".") + 1) == "pda") {
+    ret.type = PDA;
+  } else {
+    throw runtime_error("Invalid file type");
+  }
+  ifstream file(file_path);
+  if (!file.is_open()) {
+    string error = "Unable to open file: " + file_path;
+    throw runtime_error(error);
+  }
+  stringstream buffer;
+  buffer << file.rdbuf();
+  ret.content = buffer.str();
+  return ret;
+}
+// End file reading
 int main(int argc, char *argv[])
 {
-  Logger logger;
-  logger.setLogToStderr(true);
+  Logger debug_logger;
+  debug_logger.setLogToStderr(true);
+  struct Args args;
 
   try {
-    struct Args args = parse_args(argc, argv, logger);
-    print_args(args, logger);
+    args = parse_args(argc, argv, debug_logger);
+    print_args(args, debug_logger);
+  } catch (const exception &e) {
+    cerr << "Error: " << e.what() << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  try {
+    struct fla_content content = load_fla_file(args.file_path);
+    debug_logger << "File type: " << content.type << "\nFile content: " << content.content << endl;
+
   } catch (const exception &e) {
     cerr << "Error: " << e.what() << endl;
     exit(EXIT_FAILURE);
